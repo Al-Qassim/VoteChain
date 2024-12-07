61788600d0f7a40bf7d5224ced5ed80b598d1367
hadeel <e_hsaad@earthlink.iq>
2024-12-07 17:27:52 +0300

    reset password pages

--- app.py
+++ app.py
@@ -26,6 +26,13 @@ def homepage():
 def loginn():
     return render_template("login.html")
 
+@app.route('/forgetPassword')
+def forgetPassword():
+    return render_template("forgetPassword.html")
+
+@app.route('/reset_password')
+def resetPassword():
+    return render_template("reset_password.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
