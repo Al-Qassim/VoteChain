8d6a6c361962b18939469d9b18af2d8d01398ffb
Al-Qassim <qassimali624@gmail.com>
2024-12-13 09:30:46 +0300

    new chanes, images works now, but we still need some work on the backend

--- app.py
+++ app.py
@@ -1,21 +1,125 @@
-from flask import Flask, render_template, request, jsonify
+from server import *
 
+# Configure application
 app = Flask(__name__)
 
-@app.route('/sign_up')
+# Custom filter
+# app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db_path = ""
+db = SQL("sqlite:///Database.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
 def index():
-    image_url = 'C:\\Users\\Hadeel\\Downloads\\voteChain.jpg'
-    return render_template('sign_up.html',image_url=image_url)
+    """Show portfolio of stocks"""
+    # TODO
+    if request.method == "GET":
+        return render_template("homepage.html",image_url='..\\images\\voteChain.jpg', image_url1='..\\images\\2024-us-election.png')
+    elif request.method == "POST":
+        pass
 
-@app.route('/homepage',methods=['POST','GET'])
-def homepage():
-    image_url = 'C:\\Users\\Hadeel\\Downloads\\voteChain.jpg'
-    image_url1 = 'https://e3.365dm.com/24/08/1600x900/skynews-2024-us-election-teaser_6671376.png?20240830164706'
-    return render_template("homepage.html",image_url=image_url,image_url1=image_url1)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
 
-@app.route('/login')
-def loginn():
-    return render_template("login.html")
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # User reached route via GET (as by clicking a link or via redirect)
+    if request.method == "GET":
+        return render_template("register.html")
+
+    # User reached route via POST (as by submitting a form via POST)
+    elif request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username doesn't exist
+        if len(rows) != 0:
+            return apology("username already taken", 400)
+
+        # Ensure same password
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("not the same password", 400)
+
+        # record user name and hashed password
+        db.execute(
+            "insert into users (username, hash) values (?, ?)",
+            request.form.get("username"),
+            generate_password_hash( request.form.get("password") )
+        )
+
+        # Redirect user to login
+        return redirect("/login")
 
 @app.route('/forgetPassword')
 def forgetPassword():
@@ -23,12 +127,4 @@ def forgetPassword():
 
 @app.route('/reset_password')
 def resetPassword():
-    return render_template("reset_password.html",image_url="./voteChain.jpg")
-
-
-@app.route("/voteChain.jpg")
-def image():
-    return render_template("voteChain.jpg")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template("reset_password.html",image_url="images/voteChain.jpg")
